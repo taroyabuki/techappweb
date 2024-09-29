57f1f083c1e21a33747d04610491275244a096dc
ybktr <taro.yabuki@it-chiba.ac.jp>
2024-09-29 15:33:00 +0900

    9章更新

--- 07-09/html/session-none.php
+++ 07-09/html/session-none.php
@@ -4,11 +4,15 @@ session_start();
 if (isset($_GET['username'])) {
   session_destroy();
   $params = session_get_cookie_params();
-  $params['secure'] = true;
-  $params['samesite'] = 'None';
+  $params['secure'] = true; // HTTPSでのみ送信される。
+  //$params['samesite'] = 'Strict'; //aでも送信されない。
+  //$params['samesite'] = 'Lax';    //imgやiframeでは送信されない。
+  $params['samesite'] = 'None';     //別ドメインにも送信される。
+  //$params['httponly'] = true; // JavaScriptからのアクセスを禁止する。
   session_set_cookie_params($params);
   session_start();
   session_regenerate_id(true);
+  
   $_SESSION['username'] = $_GET['username'];
 }
 
