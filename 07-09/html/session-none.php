0d7d2a8e7f3059695370fefdedfb579e7b0a9c56
ybktr <taro.yabuki@it-chiba.ac.jp>
2024-09-29 14:26:31 +0900

    9章更新

--- 07-09/html/session-none.php
+++ 07-09/html/session-none.php
@@ -1,12 +1,14 @@
 <?php
-$params = session_get_cookie_params();
-$params['secure'] = true;
-$params['samesite'] = 'None';
-$params['httponly'] = true;
-session_set_cookie_params($params);
 session_start();
 
 if (isset($_GET['username'])) {
+  session_destroy();
+  $params = session_get_cookie_params();
+  $params['secure'] = true;
+  $params['samesite'] = 'None';
+  session_set_cookie_params($params);
+  session_start();
+  session_regenerate_id(true);
   $_SESSION['username'] = $_GET['username'];
 }
 
