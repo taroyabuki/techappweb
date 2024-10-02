cad29b307fc2f0732fe53d99aa69340773dbdd2d
ybktr <taro.yabuki@it-chiba.ac.jp>
2024-10-03 00:07:36 +0900

    Update session-none.php

--- 07-09/html/session-none.php
+++ 07-09/html/session-none.php
@@ -1,18 +1,14 @@
 <?php
+$params = session_get_cookie_params();
+$params['secure'] = true; // HTTPSでのみ送信される（localhostは例外）。
+//$params['samesite'] = 'Strict'; //aでも送信されない。
+//$params['samesite'] = 'Lax';    //imgやiframeでは送信されない。
+$params['samesite'] = 'None';     //別ドメインにも送信される。
+session_set_cookie_params($params);
 session_start();
 
 if (isset($_GET['username'])) {
-  session_destroy();
-  $params = session_get_cookie_params();
-  $params['secure'] = true; // HTTPSでのみ送信される。
-  //$params['samesite'] = 'Strict'; //aでも送信されない。
-  //$params['samesite'] = 'Lax';    //imgやiframeでは送信されない。
-  $params['samesite'] = 'None';     //別ドメインにも送信される。
-  //$params['httponly'] = true; // JavaScriptからのアクセスを禁止する。
-  session_set_cookie_params($params);
-  session_start();
-  session_regenerate_id(true);
-  
+  session_regenerate_id(true); // セッションを作り直す。
   $_SESSION['username'] = $_GET['username'];
 }
 
