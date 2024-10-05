c6662a7f9db71078142ed3e4afbbe13bebc411d6
ybktr <taro.yabuki@it-chiba.ac.jp>
2024-10-05 21:29:45 +0900

    9章更新

---
+++ 07-09/html/session-lax.php
@@ -0,0 +1,17 @@
+<?php
+$params = session_get_cookie_params();
+$params['secure'] = true; // HTTPSでのみ送信される（localhostは例外）。
+//$params['samesite'] = 'Strict'; //aでも送信されない。
+$params['samesite'] = 'Lax';    //imgやiframeでは送信されない。
+//$params['samesite'] = 'None';     //別ドメインにも送信される。
+session_set_cookie_params($params);
+session_start();
+
+if (isset($_GET['username'])) {
+  session_regenerate_id(true); // セッションを作り直す。
+  $_SESSION['username'] = $_GET['username'];
+}
+
+if (isset($_SESSION['username'])) {
+  echo "Hello, {$_SESSION['username']}.\n";
+}
